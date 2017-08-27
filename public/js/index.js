972645fe8f8b8654ead23ef5287ab3555df298d7
asifali22 <asifalimailme@gmail.com>
2017-08-28 00:31:57 +0530

    Basic chat app finished

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,26 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log('newMessage', message);
+    let li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    // Render
+    jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage', {
+//     from: 'Dad',
+//     text: 'Hello'
+// }, function(data) {
+//     console.log('Got it', data);
+// });
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    });
 });
