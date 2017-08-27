debd93e476bea2d465dbb1d16f8fd99c921291f8
asifali22 <asifalimailme@gmail.com>
2017-08-27 23:20:08 +0530

    Added event and broadcast listeners

--- server/server.js
+++ server/server.js
@@ -1,13 +1,35 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
-
 const app = express();
+const server = http.createServer(app);
+
+// Config socket IO
+const io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('createMessage', (message) => {
+        console.log('Create message', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
